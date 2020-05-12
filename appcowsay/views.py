747846aeeabd4f0bf247c93de4c020e3e583b851
Benjamin Mark <benjmark@protonmail.com>
2020-05-12 15:30:06 -0400

    cow rendering functional

--- appcowsay/views.py
+++ appcowsay/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, reverse, HttpResponseRedirect
-from django.utils import timezone
-
+from django.shortcuts import render
 import subprocess
 
 from appcowsay.models import Moo
 from appcowsay.forms import FormAddMoo
+
+
+def transform(rawmoo):
+    result = rawmoo.decode('utf-8')
+    return result
 
 
 def index(request):
@@ -17,9 +20,7 @@ def index(request):
             Moo.objects.create(
                 text=text,
             )
-            # moo = data['text']
-            p = subprocess.Popen(["cowsay", text], stdout=subprocess.PIPE)
-            moo = p.communicate()[0]
+            moo = subprocess.check_output(["cowsay", text]).decode('utf-8')
             return render(request, html, {'form': FormAddMoo,
                                           'moo': moo})
 
@@ -35,7 +36,5 @@ def errorview(request):
 
 def historyview(request):
     html = 'history.html'
-    # last = Moo.objects.all().count()
-    # moos = Moo.objects.all()[(last-3):last].order_by('-date')
     moos = Moo.objects.order_by('-date')[: 10]
     return render(request, html, {'moos': moos})
